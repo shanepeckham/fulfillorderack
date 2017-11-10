c7b32cb98544d3d0948090ce200d247d39b1c68e
shanepeckham <shanepeckham@live.com>
2017-11-10 14:26:02 +0000

    First working version of Fulfillorder - file mount and mongoDB

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "hackfulfillorder/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
